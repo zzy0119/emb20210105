f987ce25dd271126d10b23e6b38c5eee4e4403a9
Your Name <you@example.com>
2021-01-06 15:30:03 +0800

    emb

--- c_exercise/task.c
+++ c_exercise/task.c
@@ -1,3 +1,23 @@
 /*
 1. 读入一个整型变量，判断其奇偶性
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int number;
+
+	printf("请输入一个整型数:");
+	scanf("%d", &number);
+
+	// 处理
+	if (number % 2 == 0) {
+		printf("%d是一个偶数\n", number);
+	} else {
+		printf("%d是一个奇数\n", number);
+	}
+
+	return 0;
+}
+
