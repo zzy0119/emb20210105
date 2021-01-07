eaab60744fdf8fedf3d3497762c13e5e4ce345a2
Your Name <you@example.com>
2021-01-07 10:08:35 +0800

    hw

--- c_exercise/homework.c
+++ c_exercise/homework.c
@@ -11,4 +11,55 @@
 4. 读入一个月份, 判断输入哪个季节
 
 */
+#include <stdio.h>
+
+int main(void)
+{
+/*
+	int m, n;
+	
+	printf("请输入两个变量:");
+	scanf("%d%d", &m, &n);
+	
+	printf("%dx%d=%d\n", m, n, m*n);
+*/
+/*
+	// 作业3
+	int year;
+
+	printf("请输入年份:");
+	scanf("%d", &year);
+
+	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+		printf("%d年是闰年\n", year);
+	} else {
+		printf("%d年是平年\n", year);
+	}
+*/
+	int month;
+	
+	printf("请输入月份:");
+	scanf("%d", &month);	
+
+	if (month < 1 || month > 12) {
+		printf("没有该季节\n");
+	} else {
+		if (month >= 3 && month <= 5) {
+			printf("春天\n");
+		} else if (month > 5  && month <= 8) {
+			printf("夏天\n");
+		} else if (month > 8 && month <= 11) {
+			printf("秋天\n");
+		} else {
+			printf("冬天\n");
+		}
+	}
+
+	return 0;
+}
+
+
+
+
+
 
