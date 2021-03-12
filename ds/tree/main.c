083231318f2e35d7a22d4bfdb6bf41d730bff261
Your Name <you@example.com>
2021-03-12 09:34:05 +0800

    all

--- ds/tree/main.c
+++ ds/tree/main.c
@@ -46,11 +46,83 @@ static void treeMidTraval(const struct node_st *root)
 	treeMidTraval(root->right);
 }
 
+static void treeDestroy(struct node_st **root)
+{
+	if (*root == NULL)
+		return ;
+	treeDestroy(&(*root)->left);
+	treeDestroy(&(*root)->right);
+	printf("%d %s will be destoryed\n", (*root)->data.id, (*root)->data.name);
+	free(*root);
+	*root = NULL;
+}
+
+static void _draw(const struct node_st *root, int level)
+{
+	if (root == NULL)
+		return ;
+	_draw(root->right, level+1);
+	for (int i = 0; i < level; i++)
+		printf("        ");
+	printf("%d %s\n", root->data.id, root->data.name);
+	_draw(root->left, level+1);
+}
+
+static void treeDraw(const struct node_st *root)
+{
+	_draw(root, 0);
+}
+
+static struct node_st **_deleteFind(struct node_st **root, int id)
+{
+	if (*root == NULL)
+		return NULL;
+	if ((*root)->data.id == id)
+		return root;
+	if ((*root)->data.id >= id)
+		return _deleteFind(&(*root)->left, id);
+	else
+		return _deleteFind(&(*root)->right, id);
+}
+
+static struct node_st *treeMaxNode(const struct node_st *root)
+{
+	if (root == NULL)
+		return NULL;
+	if (root->right == NULL)
+		return (struct node_st *)root;
+	return treeMaxNode(root->right);
+}
+
+static int treeDelete(struct node_st **root, int id) 
+{
+	struct node_st **find = _deleteFind(root, id);
+	struct node_st *left, *right;
+	if (find == NULL)
+		return -1;
+	left = (*find)->left;
+	right = (*find)->right;
+	(*find)->left = (*find)->right = NULL;
+	free(*find);
+	
+	if (left != NULL) {
+		*find = left;
+		if (right != NULL) {
+			treeMaxNode(left)->right = right;	
+		}
+	} else {
+		*find = right;
+	}
+
+	return 0;
+}
+
 int main(void)
 {
 	struct node_st *t = NULL;
 	struct stu_st stu;
 	int ids[] = {5, 1, 9, 7, 4, 2, 8, 6, 3};
+	int delid;
 
 	for (int i = 0; i < sizeof(ids) / sizeof(*ids); i++) {
 		stu.id = ids[i];
@@ -59,6 +131,20 @@ int main(void)
 	}
 
 	treeMidTraval(t);
+
+	treeDraw(t);
+
+	printf("********************delete********************\n");
+	delid = 7;
+	treeDelete(&t, delid);
+	treeDraw(t);
+
+	printf("********************delete********************\n");
+	delid = 5;
+	treeDelete(&t, delid);
+	treeDraw(t);
+
+	treeDestroy(&t);
 
 	return 0;
 }
