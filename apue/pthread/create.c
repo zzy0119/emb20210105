a822cd97ba4a26ac1050eaa6d971285e4a8e027c
Your Name <you@example.com>
2021-04-06 15:01:00 +0800

    pthread

--- apue/pthread/create.c
+++ apue/pthread/create.c
@@ -4,26 +4,47 @@
 #include <unistd.h>
 #include <pthread.h>
 
+static void exit_handler1(void *s)
+{
+	printf("%s is called\n", __FUNCTION__);
+}
+
+static void exit_handler2(void *s)
+{
+	printf("%s is called\n", __FUNCTION__);
+}
+
 static void *thr_job(void *s)
 {
+	pthread_cleanup_push(exit_handler1, NULL);
+	pthread_cleanup_push(exit_handler2, NULL);
+
 	for (int i = 0; i < 5; i++) {
 		write(1, "*", 1);
 		sleep(1);
 	}
 	
-	return (void *)0;
+	// return (void *)0;
+	pthread_exit((void *)0);
+	pthread_cleanup_pop(0);
+	pthread_cleanup_pop(0);
 }
 
 int main(void)
 {
 	pthread_t tid;
 	int err;
+	pthread_attr_t attr;
 
-	err = pthread_create(&tid, NULL, thr_job, NULL);
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	err = pthread_create(&tid, &attr, thr_job, NULL);
 	if (err) {
 		fprintf(stderr, "pthread_create():%s\n", strerror(err));
 		exit(1);
 	}
+
+	// pthread_detach(tid);
 
 	for (int i = 0; i < 3; i++) {
 		write(1, "!", 1);
@@ -31,7 +52,7 @@ int main(void)
 	}
 
 	// 收尸
-	pthread_join(tid, NULL);
+	// pthread_join(tid, NULL);
 
 	pthread_exit((void *)0);
 	// return 0;
--- apue/pthread/mutex_test.c
+++ apue/pthread/mutex_test.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define THRNR	5
 
@@ -14,6 +15,7 @@ static int cur_id = 0;
 static pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
+static pthread_t tids[THRNR] = {};
 static void *thr_job(void *s)
 {
 	int id = (int)s;
@@ -32,16 +34,31 @@ static void *thr_job(void *s)
 
 }
 
+static void alrm_handler(int s)
+{
+	for (int i = 0; i < THRNR; i++)	
+		pthread_cancel(tids[i]);
+	for (int i = 0; i < THRNR; i++)
+		pthread_join(tids[i], NULL);
+
+	pthread_mutex_destroy(&mut);
+	pthread_cond_destroy(&cond);
+	exit(0);
+}
+
 int main(void)
 {
-	pthread_t tids[THRNR] = {};
 	int err;
+	struct sigaction act;
 
 	for (int i = 0; i < THRNR; i++) {
 		err = pthread_create(tids+i, NULL, thr_job, (void *)i);
 		// if error;
 	}
-
+		
+	act.sa_handler = alrm_handler;
+	act.sa_flags = 0;
+	sigaction(SIGALRM, &act, NULL);
 	alarm(5);
 
 	for (int i = 0; i < THRNR; i = (i+1) % THRNR) {
